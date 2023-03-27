15583156768ab3f7edea659ab93a254e87772e0f
SiddhantSonkar <124968287+SiddhantSonkar@users.noreply.github.com>
2023-03-28 01:21:56 +0530

    Add files via upload

---
+++ int float(sum,difference).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{   
+    int a,b,sum ,difference;
+    float x,y,S,D;
+    scanf("%d%d",&a,&b);
+    scanf("%f%f",&x,&y);
+    sum = a+b;
+    difference =a-b;
+    S=x+y;
+    D=x-y;
+    
+    printf("%d%d",sum,difference);
+    printf("\n%f%f",S,D);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
